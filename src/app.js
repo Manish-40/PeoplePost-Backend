11f70da3e5d2059689e09306cbe756b69a34147d
apurva470 <apurvasolanki70@gmail.com>
2025-12-14 16:40:45 +0530

    add cors url

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const path =require("path");
 require('dotenv').config()
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_ALLOWED_URL,
     credentials: true,
 }));
 app.use(express.json());
