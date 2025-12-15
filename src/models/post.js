b135aa09efe62f198f84e1c5042557acec7f83c7
apurva470 <apurvasolanki70@gmail.com>
2025-12-15 14:02:34 +0530

    changing in post schema

--- src/models/post.js
+++ src/models/post.js
@@ -1,53 +1,118 @@
+// const mongoose = require("mongoose");
+// const validator = require("validator");
+
+// const postschema = new mongoose.Schema({
+//     url: {
+//         type: String,
+//         // validate(value) {
+//         //     if (!validator.isURL(value)) {
+//         //         throw new Error("invalid url" + value);
+//         //     }
+//         // }
+//     },
+//     description: {
+//         type: String,
+//         default: "this is a default description of post",
+//     },
+//     author: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "user",
+//         required: true,
+//     },
+//     status: {
+//         type: String,
+//         default: "posttoall"
+//     },
+//     name: {
+//         type: String,
+//         required: true,
+//     },
+//     photourl: {
+//         type: String,
+//         default: "https://media.istockphoto.com/id/1131164548/vector/avatar-5.jpg",
+//         validate(value) {
+//             // Allow localhost URLs or valid URLs
+//             if (!value.startsWith("http://localhost") && !validator.isURL(value, { require_protocol: true })) {
+//                 throw new Error("invalid url " + value);
+//             }
+//         }
+//     },
+//     userPostViewCount:{
+//         type:Number,
+//         default: 0
+//     },
+//     viewedBy:[
+//         {
+//             type:mongoose.Schema.Types.ObjectId,
+//             ref:"user"
+//         }
+//     ]
+// }, { timestamps: true });
+
+// const Usermodel = mongoose.model("post", postschema);
+// module.exports = Usermodel;
+
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const postschema = new mongoose.Schema({
+const postschema = new mongoose.Schema(
+  {
     url: {
-        type: String,
-        // validate(value) {
-        //     if (!validator.isURL(value)) {
-        //         throw new Error("invalid url" + value);
-        //     }
-        // }
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isURL(value, { require_protocol: true })) {
+          throw new Error("Invalid post image URL: " + value);
+        }
+      },
     },
+
     description: {
-        type: String,
-        default: "this is a default description of post",
+      type: String,
+      default: "this is a default description of post",
     },
+
     author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+
+    status: {
+      type: String,
+      default: "posttoall",
+    },
+
+    name: {
+      type: String,
+      required: true,
+    },
+
+    photourl: {
+      type: String,
+      default:
+        "https://media.istockphoto.com/id/1131164548/vector/avatar-5.jpg",
+      validate(value) {
+        if (!validator.isURL(value, { require_protocol: true })) {
+          throw new Error("Invalid profile photo URL: " + value);
+        }
+      },
+    },
+
+    userPostViewCount: {
+      type: Number,
+      default: 0,
+    },
+
+    viewedBy: [
+      {
         type: mongoose.Schema.Types.ObjectId,
         ref: "user",
-        required: true,
-    },
-    status: {
-        type: String,
-        default: "posttoall"
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    photourl: {
-        type: String,
-        default: "https://media.istockphoto.com/id/1131164548/vector/avatar-5.jpg",
-        validate(value) {
-            // Allow localhost URLs or valid URLs
-            if (!value.startsWith("http://localhost") && !validator.isURL(value, { require_protocol: true })) {
-                throw new Error("invalid url " + value);
-            }
-        }
-    },
-    userPostViewCount:{
-        type:Number,
-        default: 0
-    },
-    viewedBy:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"user"
-        }
-    ]
-}, { timestamps: true });
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-const Usermodel = mongoose.model("post", postschema);
-module.exports = Usermodel;
+module.exports = mongoose.model("post", postschema);
+
