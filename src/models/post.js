e56a60f1c6f372ce1e1f59776bdab6def5a02805
apurva470 <apurvasolanki70@gmail.com>
2025-12-16 13:23:46 +0530

    post schema url requred true removing so only description post

--- src/models/post.js
+++ src/models/post.js
@@ -59,7 +59,7 @@ const postschema = new mongoose.Schema(
   {
     url: {
       type: String,
-      required: true,
+      // required: true,
       validate(value) {
         if (!validator.isURL(value, { require_protocol: true })) {
           throw new Error("Invalid post image URL: " + value);
